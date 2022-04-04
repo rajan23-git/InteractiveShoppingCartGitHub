0e6ab7cd5bc4f61b3817c59294faf61ce842077e
rajan23-git <rajan23@up.edu>
2022-04-03 20:09:27 -0700

    Initial Commit

---
+++ InteractiveShoppingCart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InteractiveShoppingCart
+//
+//  Created by Pranav Rajan on 4/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InteractiveShoppingCart/InteractiveShoppingCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveShoppingCartApp.swift
+//  InteractiveShoppingCart
+//
+//  Created by Pranav Rajan on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveShoppingCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
